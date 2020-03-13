8e4419ca2ad47847a0ce2e279b89a975acef7d6b
91074 <910749592@qq.com>
2020-03-13 18:30:10 +0800

    一些改动

--- src/main/java/com/wqs/jsd/service/impl/UserHeadSculptureServiceImpl.java
+++ src/main/java/com/wqs/jsd/service/impl/UserHeadSculptureServiceImpl.java
@@ -83,22 +83,6 @@ public class UserHeadSculptureServiceImpl implements UserHeadSculptureService {
 
     @Override
     public ResultBean<String> getImgUrl(String s, HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
         try {
             int staffId = Integer.parseInt(s);
             System.out.println(staffId);
@@ -106,8 +90,12 @@ public class UserHeadSculptureServiceImpl implements UserHeadSculptureService {
             System.out.println(userId);
             String imgUrl = mapper.selectByUserId(userId);
             System.out.println(imgUrl);
-            System.out.println(ip);
-            imgUrl = ip + ":8080" + imgUrl;
+            String addr = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(addr);
+            System.out.println(request.getRequestURL());
+            System.out.println(request.getRemoteAddr());
+            System.out.println(request.getRemoteHost());
+            imgUrl = "http://39.107.49.176:8080" + imgUrl;
             System.out.println(imgUrl);
             return new ResultBean<>(imgUrl, SUCCESS, "success");
         } catch (Exception e) {
